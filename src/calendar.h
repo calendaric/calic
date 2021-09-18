1ddf57b208d36df28495cb481aa008a03a3a4049
Ivan Azoyan <azoyan.89@mail.ru>
2021-09-18 14:53:48 +0300

    add scroll with-left-right arrows

--- src/calendar.h
+++ src/calendar.h
@@ -2,17 +2,41 @@
 #include <stdbool.h>
 #include <time.h>
 
-typedef struct {
+typedef struct
+{
     int8_t week[6][7];
     int8_t weekNumber;
     int8_t currentDayNumber;
 } Calendar;
 
+typedef enum
+{
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May_ = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
+} Month;
+typedef enum
+{
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+} WeekDay;
 
-typedef enum { January = 1, Febrary = 2, March = 3, April = 4, May = 5, June = 6, July = 7, August = 8, September = 9, October = 10, November = 11, December = 12 } Month;
-typedef enum { Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7 } WeekDay;
-
-typedef struct {
+typedef struct
+{
     int seconds;
     int minutes;
     int hours;
@@ -23,6 +47,14 @@ typedef struct {
     int yearDay;
 } Time;
 
-Time convertTime(const struct tm* tm);
+int getNumberOfDays(int month, int year);
 
-Calendar createCalendar(const Time* currentTime);
+Time convertTime(const struct tm *tm);
+
+void sub_one_month(struct tm *tm);
+
+void add_one_month(struct tm *tm);
+
+Calendar createCalendar(const Time *currentTime);
+
+bool isLeap(int year);
