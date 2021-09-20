fb53d838bebf1d457c32350bd7dbf6e2e083c136
Ivan Azoyan <azoyan.89@mail.ru>
2021-09-21 00:08:17 +0300

    add windows compatibily

--- src/compatibility.h
+++ src/compatibility.h
@@ -1,55 +1,121 @@
+#pragma once
+
 #include <string.h>
 #include <stdlib.h>
 
-#if defined(__WIN32__) || defined(__WIN64__)
+#if defined(__WIN32__) || defined(__WIN64__) || defined(__CYGWIN32__) || defined(__CYGWIN64__) || defined(_MSVC_TRADITIONAL)
 #include <conio.h>
+#include <Windows.h>
+
+struct tm* local_time(time_t *const time) {
+	struct tm timeinfo;
+	localtime_s(&timeinfo, time);
+	return &timeinfo;
+}
+
+#include <windows.h>
+
+enum KeyCodes
+{
+	CtrlC = 3,
+	CtrlD = 4,
+	CtrlZ = 26,
+	Esc = 27,
+	Left = 75,
+	Right = 77,
+};
+
+void microsec_sleep(int waitTime) {
+	__int64 time1 = 0, time2 = 0, freq = 0;
+
+	QueryPerformanceCounter((LARGE_INTEGER*)& time1);
+	QueryPerformanceFrequency((LARGE_INTEGER*)& freq);
+
+	do {
+		QueryPerformanceCounter((LARGE_INTEGER*)& time2);
+	} while ((time2 - time1) < waitTime);
+}
+
+int keyboard_hit() {
+	return _kbhit();
+}
+
+int get_char() {
+	return _getch();
+}
+
+void atomic_decrement(long int* n) {
+	InterlockedDecrement(n);
+}
+
+
 #elif defined(unix)
 #include <unistd.h>
 #include <sys/select.h>
 #include <termios.h>
 #include <unistd.h>
 
+enum KeyCodes
+{
+	CtrlC = 3,
+	CtrlD = 4,
+	CtrlZ = 26,
+	Esc = 27,
+	Left = 68,
+	Right = 67,
+};
+
 struct termios orig_termios;
 
 void reset_terminal_mode()
 {
-    tcsetattr(0, TCSANOW, &orig_termios);
+	tcsetattr(0, TCSANOW, &orig_termios);
 }
+
+void microsec_sleep(int microseconds) {
+	usleep(microseconds);
+}
+
+
+void atomic_decrement(long int* n) {
+	__sync_fetch_and_sub(&keepRunning_, 1);
+}
+
 
 void set_conio_terminal_mode()
 {
-    struct termios new_termios;
+	struct termios new_termios;
 
-    /* take two copies - one for now, one for later */
-    tcgetattr(0, &orig_termios);
-    memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+	/* take two copies - one for now, one for later */
+	tcgetattr(0, &orig_termios);
+	memcpy(&new_termios, &orig_termios, sizeof(new_termios));
 
-    /* register cleanup handler, and set the new terminal mode */
-    atexit(reset_terminal_mode);
-    cfmakeraw(&new_termios);
-    tcsetattr(0, TCSANOW, &new_termios);
+	/* register cleanup handler, and set the new terminal mode */
+	atexit(reset_terminal_mode);
+	cfmakeraw(&new_termios);
+	tcsetattr(0, TCSANOW, &new_termios);
 }
 
-int kbhit()
+int keyboard_hit()
 {
-    struct timeval tv = {0L, 0L};
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(0, &fds);
-    return select(1, &fds, NULL, NULL, &tv);
+	struct timeval tv = { 0L, 0L };
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(0, &fds);
+	return select(1, &fds, NULL, NULL, &tv);
 }
 
-int getch()
+int get_char()
 {
-    int r = 0;
-    unsigned char c = 0;
-    if ((r = read(0, &c, sizeof(c))) < 0)
-    {
-        return r;
-    }
-    else
-    {
-        return c;
-    }
+	int r = 0;
+	unsigned char c = 0;
+	if ((r = read(0, &c, sizeof(c))) < 0)
+	{
+		return r;
+	}
+	else
+	{
+		return c;
+	}
 }
 #endif
--- src/main.c
+++ src/main.c
@@ -10,178 +10,173 @@ static volatile sig_atomic_t keepRunning_ = 1;
 
 static void interruptHandler(int n)
 {
-    (void)n;
-    __sync_fetch_and_sub(&keepRunning_, 1);
+	(void)n;
+	// __sync_fetch_and_sub(&keepRunning_, 1);
+		(&keepRunning_);
 }
 
-static void drawTimebar(const struct tm *timeinfo, char *buffer, size_t len)
+static void drawTimebar(const struct tm* timeinfo, char* buffer, size_t len)
 {
-    strftime(buffer, len, "%d.%m.%Y   %X", timeinfo);
-    printf("%s\n\r", buffer);
+	strftime(buffer, len, "%d.%m.%Y   %X", timeinfo);
+	printf("%s\n\r", buffer);
 }
 
-static void drawCalendar(const struct tm *timeinfo)
+static void drawCalendar(const struct tm* timeinfo)
 {
-    const Time currentTime = convertTime(timeinfo);
-    Calendar calendar = createCalendar(&currentTime);
-    const uint8_t weeksCount = sizeof(calendar.week) / sizeof(calendar.week[0]);
-    printf("%3s%3s%3s%3s%3s%3s%3s", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
-    printf("\n\r");
-    size_t month_counter = 0;
+	const Time currentTime = convertTime(timeinfo);
+	Calendar calendar = createCalendar(&currentTime);
+	const uint8_t weeksCount = sizeof(calendar.week) / sizeof(calendar.week[0]);
+	printf("%3s%3s%3s%3s%3s%3s%3s", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
+	printf("\n\r");
+	size_t month_counter = 0;
 
-    for (uint8_t i = 0; i < weeksCount; ++i)
-    {
-        for (uint8_t j = 0; j < sizeof(calendar.week[i]); ++j)
-        {
-            const uint8_t d = calendar.week[i][j];
-            if (d == calendar.currentDayNumber)
-            {
-                printf(" \033[7m");
-                printf("%*d", 2, d);
-                printf("\033[27m");
-                continue;
-            }
-            if (d == 1 && month_counter == 0)
-            {
-                month_counter++;
-                printf("\033[1m");
-            }
-            else if (d == 1 && month_counter == 1)
-            {
-                month_counter++;
-            }
-            if (month_counter == 1)
-            {
-                printf("\033[1m");
-            }
-            else
-            {
-                printf("\033[0m");
-            }
-            printf("%3d", d);
-        }
-        printf("\n\r");
-    }
+	for (uint8_t i = 0; i < weeksCount; ++i)
+	{
+		for (uint8_t j = 0; j < sizeof(calendar.week[i]); ++j)
+		{
+			const uint8_t d = calendar.week[i][j];
+			if (d == calendar.currentDayNumber)
+			{
+				printf(" \033[7m");
+				printf("%*d", 2, d);
+				printf("\033[27m");
+				continue;
+			}
+			if (d == 1 && month_counter == 0)
+			{
+				month_counter++;
+				printf("\033[1m");
+			}
+			else if (d == 1 && month_counter == 1)
+			{
+				month_counter++;
+			}
+			if (month_counter == 1)
+			{
+				printf("\033[1m");
+			}
+			else
+			{
+				printf("\033[0m");
+			}
+			printf("%3d", d);
+		}
+		printf("\n\r");
+	}
 }
 
 int help()
 {
-    printf("calic - \033[1mcal\033[0mendar \033[1mi\033[0mntera\033[1mc\033[0mtive\n");
-    printf("\n");
-    printf("Usage:\n");
-    printf("    left / right arrows - scroll previous or next month\n");
-    printf("    'q'                 - quit\n");
-    printf("\n");
-    printf("License: MIT\n");
-    printf("Sources: https://github.com/calendaric/calic\n");
-    printf("2021 Ivan Azoyan <ivan.azoyan@gmail.com>\n");
-    return 0;
+	printf("calic - \033[1mcal\033[0mendar \033[1mi\033[0mntera\033[1mc\033[0mtive\n");
+	printf("\n");
+	printf("Usage:\n");
+	printf("    left / right arrows - scroll previous or next month\n");
+	printf("    'q'                 - quit\n");
+	printf("\n");
+	printf("License: MIT\n");
+	printf("Sources: https://github.com/calendaric/calic\n");
+	printf("2021 Ivan Azoyan <ivan.azoyan@gmail.com>\n");
+	return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    if (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))
-    {
-        return help();
-    }
+	if (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))
+	{
+		return help();
+	}
 
-    signal(SIGINT, *interruptHandler);
-    signal(SIGABRT, *interruptHandler);
-    set_conio_terminal_mode();
+	signal(SIGINT, *interruptHandler);
+	signal(SIGABRT, *interruptHandler);
+#ifdef UNIX
+	set_conio_terminal_mode();
+#endif
 
-    time_t now = time(0);
+	time_t now = time(0);
 
-    struct tm timeinfo = *localtime(&now);
-    struct tm calendar_info = *localtime(&now);
+	struct tm timeinfo = *local_time(&now);
+	struct tm calendar_info = *local_time(&now);
 
-    char buffer[80];
-    int month_cursor = 0;
-    printf("\033[?25l");
+	char buffer[80];
+	int month_cursor = 0;
+	printf("\033[?25l");
 
-    int request_time_counter = 0;
+	int request_time_counter = 0;
 
-    while (keepRunning_)
-    {
-        if (request_time_counter == 0)
-        {
-            now = time(0);
-        }
-        if (month_cursor == 0)
-        {
-            calendar_info = timeinfo;
-        }
+	while (keepRunning_)
+	{
+		if (request_time_counter == 0)
+		{
+			now = time(0);
+		}
+		if (month_cursor == 0)
+		{
+			calendar_info = timeinfo;
+		}
 
-        int keyboard_input = kbhit();
+		int keyboard_input = keyboard_hit();
 
-        if (keyboard_input)
-        {
-            request_time_counter = 0;
-            now = time(0);
-            int c = getch();
-            enum
-            {
-                CtrlC = 3,
-                CtrlD = 4,
-                CtrlZ = 26,
-                Esc = 27,
-                Left = 68,
-                Right = 67,
-            };
-            if (c == CtrlC || c == CtrlD || c == CtrlZ || c == 'q')
-            {
-                printf("\n\r");
-                printf("\033[");
-                printf("%d", 2);
+		if (keyboard_input)
+		{
+			request_time_counter = 0;
+			now = time(0);
+			int c = get_char();
+			
+			if (c == CtrlC || c == CtrlD || c == CtrlZ || c == 'q')
+			{
+				printf("\n\r");
+				printf("\033[");
+				printf("%d", 2);
 
-                printf("A");
-                break;
-            }
+				printf("A");
+				break;
+			}
 
-            switch (c)
-            {
-            case Left:
-                month_cursor -= 1;
-                sub_one_month(&calendar_info);
-                break;
-            case Right:
-                month_cursor += 1;
-                add_one_month(&calendar_info);
-            default:
-                break;
-            }
-        }
-        if (request_time_counter == 0)
-        {
-            timeinfo = *localtime(&now);
+			switch (c)
+			{
+			case Left:
+				month_cursor -= 1;
+				sub_one_month(&calendar_info);
+				break;
+			case Right:
+				month_cursor += 1;
+				add_one_month(&calendar_info);
+			default:
+				break;
+			}
+		}
+		if (request_time_counter == 0)
+		{
+			timeinfo = *local_time(&now);
 
-            if (month_cursor != 0)
-            {
-                timeinfo.tm_mon = calendar_info.tm_mon;
-            }
-            void shift_month(time_t * now, struct tm * current, int month_cursor);
+			if (month_cursor != 0)
+			{
+				timeinfo.tm_mon = calendar_info.tm_mon;
+			}
+			void shift_month(time_t * now, struct tm* current, int month_cursor);
 
-            drawTimebar(&timeinfo, buffer, sizeof(buffer));
-            drawCalendar(&calendar_info);
+			drawTimebar(&timeinfo, buffer, sizeof(buffer));
+			drawCalendar(&calendar_info);
 
-            printf("\033[");
-            printf("%d", 8);
-            printf("A");
-        }
-        const int sleep_time = 20 * 1000;
-        request_time_counter += sleep_time;
-        usleep(sleep_time);
-        request_time_counter %= 1000000;
-    }
+			printf("\033[");
+			printf("%d", 8);
+			printf("A");
+		}
+		const int sleep_time = 20 * 1000;
+		request_time_counter += sleep_time;
+		microsec_sleep(sleep_time);
+		request_time_counter %= 1000000;
+	}
 
-    for (int i = 0; i < 9; ++i)
-    {
-        printf("                                                                  \n\r");
-    }
+	for (int i = 0; i < 9; ++i)
+	{
+		printf("                                                                  \n\r");
+	}
 
-    printf("\b\r");
-    printf("\033[");
-    printf("%d", 9);
-    printf("A");
-    printf("\033[?25h");
-    return 0;
+	printf("\b\r");
+	printf("\033[");
+	printf("%d", 9);
+	printf("A");
+	printf("\033[?25h");
+	return 0;
 }
